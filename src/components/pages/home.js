e58c7b907ad4735e6fd94bbed09402f62c6da779
MrGio7 <lashagiorgi1233@gmail.com>
2021-11-14 22:16:19 +0400

    imported particles

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import greetingImg from "../../assets/img/greeting.jpg";
+import Particles from "react-tsparticles";
 
 class Home extends React.Component {
   constructor(props) {
@@ -22,7 +23,107 @@ class Home extends React.Component {
               technical systems to create impact.
             </p>
           </div>
-            <img src={greetingImg} alt="greeting" />
+          <Particles
+            id="tsparticles"
+            options={{
+              position: "relative",
+              background: {
+                color: {
+                  value: "#ffffff",
+                },
+                image: `url(${greetingImg})`,
+                position: "relative",
+                repeat: "no-repeat",
+                size: "cover",
+              },
+              backgroundMask: {
+                cover: {
+                  color: {
+                    value: "#32144f",
+                  },
+                },
+                enable: true,
+              },
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "bubble",
+                    parallax: {
+                      force: 60,
+                    },
+                  },
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 1,
+                    size: 100,
+                  },
+                  grab: {
+                    distance: 400,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#ffffff",
+                },
+                links: {
+                  color: {
+                    value: "#ffffff",
+                  },
+                  distance: 150,
+                  enable: true,
+                },
+                move: {
+                  attract: {
+                    rotate: {
+                      x: 600,
+                      y: 1200,
+                    },
+                  },
+                  enable: true,
+                  outModes: {
+                    bottom: "out",
+                    left: "out",
+                    right: "out",
+                    top: "out",
+                  },
+                },
+                number: {
+                  density: {
+                    enable: true,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  animation: {
+                    speed: 1,
+                    minimumValue: 0.1,
+                  },
+                },
+                size: {
+                  random: {
+                    enable: true,
+                  },
+                  value: {
+                    min: 1,
+                    max: 30,
+                  },
+                  animation: {
+                    speed: 40,
+                    minimumValue: 0.1,
+                  },
+                },
+              },
+            }}
+          />
         </div>
       </div>
     );
