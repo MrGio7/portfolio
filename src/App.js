fe9911da5414e9f572a88feb57b42e2b62813bb0
MrGio7 <lashagiorgi1233@gmail.com>
2021-11-14 12:41:44 +0400

    added navbar component and implemented router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <div className="App">
-      Fresh Start
-    </div>
-  );
+import React from 'react';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="navBar">
+          <div className="logo">Logo</div>
+          <div className="navigation">
+            <ul>
+              <li>Home</li>
+              <li>Education</li>
+              <li>Experience</li>
+              <li>Projects</li>
+              <li>Contact Me</li>
+              <li>Language</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
