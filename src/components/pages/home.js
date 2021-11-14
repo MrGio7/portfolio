feba039af1be4c0aff048cbd63e57890f80d026e
MrGio7 <lashagiorgi1233@gmail.com>
2021-11-14 17:12:46 +0400

    added home content

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import greetingImg from "../../assets/img/greeting.jpg";
 
 class Home extends React.Component {
   constructor(props) {
@@ -10,7 +11,21 @@ class Home extends React.Component {
   }
 
   render() {
-    return <div className="home">Home</div>;
+    return (
+      <div className="home">
+        <div className="greeting">
+          <div className="text">
+            <h1>Giorgi Bakashvili</h1>
+            <p>
+              A passionate individual who always thrive to work on end to end
+              products which develop sustainable and scalable social and
+              technical systems to create impact.
+            </p>
+          </div>
+            <img src={greetingImg} alt="greeting" />
+        </div>
+      </div>
+    );
   }
 }
 
