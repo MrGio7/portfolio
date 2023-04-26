0a8fa58f09e772329d54ee968e947b55d88ffa91
Giorgi Bakashvili <lashagiorgi1233@gmail.com>
2023-04-26 10:28:06 +0400

    init

--- src/components/loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Icon } from '@iconify/react';
-
-export default function Loading() {
-    return <div className="loading">
-        <Icon icon="eos-icons:bubble-loading" />
-    </div>
-}
